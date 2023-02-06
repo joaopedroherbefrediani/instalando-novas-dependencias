cb19949ebccc7d59e6b09f714e96e7ead5b804a7
Frediani <51144585+joaofrediani@users.noreply.github.com>
2023-02-06 16:26:19 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -14,24 +14,44 @@ choco --version
 Write-Host "Atualizando Chocolatey"
 choco upgrade chocolatey
 
+Write-Host "Instalando Adobe Acrobat Reader DC"
+cinst adobereader --version 2022.003.20314 -y
+
 Write-Host "Instalando Google Chrome"
-cinst googlechrome --version=103.0.5060.114 -Y
+cinst googlechrome --version 109.0.5414.120 -y
 
-Write-Host "Instalando Java (JDK)"
-cinst jdk8 --version=8.0.231 -Y
+Write-Host "Instalando Microsoft Visual C++ Redistributable for Visual Studio 2015-2022"
+cinst vcredist140 --version 14.34.31931 -y
 
-Write-Host "Instalando Winrar"
-cinst winrar --version=6.11.0.20220504 -Y
+Write-Host "Instalando Notepad++"
+cinst notepadplusplus.install --version 8.4.9 -y
+
+Write-Host "Instalando Microsoft .NET Framework 4.8"
+cinst dotnetfx --version 4.8.0.20220524 -y
 
 Write-Host "Instalando Git"
-cinst git -Y
+cinst git.install --version 2.39.1 -y
 
-Write-Host "Instalando AnyDesk Exe"
-cinst anydesk.install --version=7.0.10 -Y
+Write-Host "Instalando WinRAR"
+cinst winrar --version 6.20 -y
+
+Write-Host "Instalando Node JS"
+cinst nodejs.install --version 19.6.0 -y
 
 Write-Host "Instalando CCleaner"
-cinst ccleaner --version=5.91.9537 -Y
+cinst ccleaner --version 6.08.10255 -y
 
-Write-Host "Instalando Adobe Acrobat Reader DC"
-cinst adobereader --version=2022.001.20085 -Y
+Write-Host "Instalando Visual Studio Code"
+cinst vscode --version 1.75.0 -y
+
+Write-Host "Instalando AnyDesk MSI"
+cinst anydesk.install --version 7.1.8 -y
+
+Write-Host "Instalando Java SE 8"
+cinst jdk8 --version 8.0.211 -y
+
+Write-Host "Instalando DirectX"
+cinst directx --version 9.29.1974.20210222 -y
+
+
 
