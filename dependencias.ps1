5f8f43121e9579de057d0688d4214e3df415a36c
Frediani <51144585+joaofrediani@users.noreply.github.com>
2023-03-20 08:43:32 -0300

    Atualização 20-03-2023

--- dependencias.ps1
+++ dependencias.ps1
@@ -18,28 +18,28 @@ Write-Host "Instalando Adobe Acrobat Reader DC"
 cinst adobereader --version 2022.003.20322 -y
 
 Write-Host "Instalando Google Chrome"
-cinst googlechrome --version 110.0.5481.104 -y
+cinst googlechrome --version 111.0.5563.65 -y
 
 Write-Host "Instalando Microsoft Visual C++ Redistributable for Visual Studio All Versions"
-cinst vcredist-all --version 1.0.1 -y
+cinst vcredist140 --version 14.34.31938 -y
 
 Write-Host "Instalando Notepad++"
-cinst notepadplusplus.install --version 8.4.9 -y
+cinst notepadplusplus.install --version 8.5 -y
 
 Write-Host "Instalando Git"
-cinst git --version 2.39.2 -y
+cinst git.install --version 2.40.0 -y
 
 Write-Host "Instalando WinRAR"
 cinst winrar --version 6.21 -y
 
 Write-Host "Instalando Node JS"
-cinst nodejs --version 19.7.0 -y
+cinst nodejs --version 19.8.1 -y
 
 Write-Host "Instalando CCleaner"
-cinst ccleaner --version 6.09.10300 -y
+cinst ccleaner --version 6.10.10347 -y
 
 Write-Host "Instalando Visual Studio Code"
-cinst vscode --version 1.75.1 -y
+cinst vscode --version 1.76.2 -y
 
 Write-Host "Instalando AnyDesk MSI"
 cinst anydesk.install --version 7.1.8 -y
@@ -51,7 +51,7 @@ Write-Host "Instalando DirectX"
 cinst directx --version 9.29.1974.20210222 -y
 
 Write-Host "Instalando Microsoft Teams"
-cinst microsoft-teams --version 1.6.00.1381 -y
+cinst microsoft-teams --version 1.6.00.4472 -y
 
 Write-Host "Instalando Discord"
 cinst discord --version 1.0.9005 -y
