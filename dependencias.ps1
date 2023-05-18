4b961a4121892098787ff91fe98da8fb45070c8b
Frediani <51144585+joaofrediani@users.noreply.github.com>
2023-05-18 15:42:52 -0300

    Update dependencias.ps1

--- dependencias.ps1
+++ dependencias.ps1
@@ -15,47 +15,40 @@ Write-Host "Atualizando Chocolatey"
 choco upgrade chocolatey
 
 Write-Host "Instalando Adobe Acrobat Reader DC"
-cinst adobereader --version 2023.001.20093 -y
+cinst adobereader --version 2023.001.20143 -y
 
 Write-Host "Instalando Google Chrome"
-cinst googlechrome --version 111.0.5563.111 -y
+cinst googlechrome --version 113.0.5672.127 -y
 
-Write-Host "Instalando Microsoft Visual C++ Redistributable for Visual Studio All Versions"
-cinst vcredist140 --version 14.34.31938 -y
+Write-Host "Instalando Microsoft Visual C++ 2010"
+cinst vcredist140 --version 14.36.32532 -y
 
 Write-Host "Instalando Notepad++"
-cinst notepadplusplus.install --version 8.5.1 -y
+cinst notepadplusplus.install --version 8.5.3 -y
+
+Write-Host "Instalando Microsoft Visual C++ 2015"
+cinst vcredist2015 --version 14.0.24215.20170201 -y
+
+Write-Host "Instalando Dotnet"
+cinst dotnetfx --version 4.8.0.20220524 -y
 
 Write-Host "Instalando Git"
-cinst git.install --version 2.40.0 -y
+cinst git.install --version 2.40.1 -y
 
-Write-Host "Instalando WinRAR"
+Write-Host "Instalando Winrar"
 cinst winrar --version 6.21 -y
 
-Write-Host "Instalando Node JS"
-cinst nodejs.install --version 19.8.1 -y
+Write-Host "Instalando NodeJS"
+cinst nodejs.install --version 20.2.0 -y
 
-Write-Host "Instalando CCleaner"
-cinst ccleaner --version 6.10.10347 -y
+Write-Host "Instalando Teams"
+cinst microsoft-teams --version 1.6.00.11166 -y
 
-Write-Host "Instalando Visual Studio Code"
-cinst vscode.install --version 1.76.2 -y
+Write-Host "Instalando VsCode"
+cinst vscode --version 1.78.2 -y
 
-Write-Host "Instalando AnyDesk MSI"
-cinst anydesk.install --version 7.1.9 -y
+Write-Host "Instalando Ccleaner"
+cinst ccleaner --version 6.12.10490 -y
 
-Write-Host "Instalando Java SE 8"
-cinst jre8 --version 8.0.361 -y
-
-Write-Host "Instalando DirectX"
-cinst directx --version 9.29.1974.20210222 -y
-
-Write-Host "Instalando Microsoft Teams"
-cinst microsoft-teams --version 1.6.00.4472 -y
-
-Write-Host "Instalando Discord"
-cinst discord --version 1.0.9005 -y
-
-Write-Host "Instalando LightShot"
-cinst lightshot --version 5.5.0.720221014 -y
-
+Write-Host "Instalando Java"
+cinst javaruntime --version 8.0.231 -y
