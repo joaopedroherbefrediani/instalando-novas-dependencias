224b8b20505e459234e7f93293a8c4226ce51146
Frediani <51144585+joaofrediani@users.noreply.github.com>
2023-06-28 11:58:37 -0300

    Update dependencias.ps1

--- dependencias.ps1
+++ dependencias.ps1
@@ -15,34 +15,34 @@ Write-Host "Atualizando Chocolatey"
 choco upgrade chocolatey
 
 Write-Host "Instalando Java SE 8"
-cinst jre8 --version 8.0.371 -y
+choco install jre8 --version 8.0.371 -y
 
 Write-Host "Instalando Directx 9"
-cinst directx --version 9.29.1974.20210222 -y
+choco install directx --version 9.29.1974.20210222 -y
 
 Write-Host "Instalando Visual C++ Redistributable"
-cinst vcredist140 --version 14.36.32532 -y
+choco install vcredist140 --version 14.36.32532 -y
 
 Write-Host "Instalando Adobe Reader DC"
-cinst adobereader --version 2023.3.20201.1 -y
+choco install adobereader --version 2023.3.20201.1 -y
 
 Write-Host "Instalando Notepad++"
-cinst notepadplusplus.install --version 8.5.4 -y
+choco install notepadplusplus.install --version 8.5.4 -y
 
 Write-Host "Instalando Winrar"
-cinst winrar --version 6.22 -y
+choco install winrar --version 6.22 -y
 
 Write-Host "Instalando CCleaner"
-cinst ccleaner --version 6.13.10517 -y
+choco install ccleaner --version 6.13.10517 -y
 
 Write-Host "Instalando AnyDesk"
-cinst anydesk.install --version 7.1.12 -y
+choco install anydesk.install --version 7.1.12 -y
 
 Write-Host "Instalando Lightshot"
-cinst lightshot --version 5.5.0.720221014 -y
+choco install lightshot --version 5.5.0.720221014 -y
 
 Write-Host "Instalando Microsoft Teams"
-cinst microsoft-teams --version 1.6.0.16472 -y
+choco install microsoft-teams --version 1.6.0.16472 -y
 
 Write-Host "Instalando Google Chrome"
-cinst googlechrome --version 114.0.5735.199 -y
+choco install googlechrome --version 114.0.5735.199 -y
